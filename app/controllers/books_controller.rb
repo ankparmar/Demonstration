fb0233ee59de8b12f4079567b7f52f9b9e1f72e7
ankparmar <anparmar@bestpeers.com>
2022-05-24 16:24:58 +0530

    new Heroku application

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,7 +32,7 @@ class BooksController < ApplicationController
         # format.html { redirect_to book_url(@book), notice: "Book was successfully created." }
         format.html { redirect_to books_path, notice: "Book was successfully created." }
 
-        format.json { render :show, status: :created, location: @book }
+        format.json { render :index, status: :created, location: @book }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @book.errors, status: :unprocessable_entity }
@@ -45,7 +45,7 @@ class BooksController < ApplicationController
     respond_to do |format|
       if @book.update(book_params)
         format.html { redirect_to book_url(@book), notice: "Book was successfully updated." }
-        format.json { render :show, status: :ok, location: @book }
+        format.json { render :index, status: :ok, location: @book }
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @book.errors, status: :unprocessable_entity }
