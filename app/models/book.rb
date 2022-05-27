aa275efdea685928523946ad82499a3f193ed433
ankparmar <anparmar@bestpeers.com>
2022-05-27 12:38:39 +0530

    Changes in book and user model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
   has_many :book_requests  , dependent: :destroy
-  has_many :users, through: :book_requests
+  # has_many :users, through: :book_requests
   validates :title, presence: true
   validates :author, presence: true
   validates :description, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,:trackable,:confirmable
 
   has_many :book_requests
-  has_many :books, through: :book_requests
+  # has_many :books, through: :book_requests
 
   def admin?
     user_type == 'admin'
